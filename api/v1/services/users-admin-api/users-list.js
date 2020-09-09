1469ea65878ab7cf2dc561116f0f9600a7454b10
Roman A. Shvetsov <tcp6000@gmail.com>
2020-09-09 19:16:53 +0700

    Implemented the "Users List" endpoint.

--- api/v1/services/users-admin-api/users-list.js
+++ api/v1/services/users-admin-api/users-list.js
@@ -1,7 +1,23 @@
-import {success} from '../../libs/response-lib';
+import {success, internalServerError} from '../../libs/response-lib';
+import AWS from 'aws-sdk';
+
+const provider = new AWS.CognitoIdentityServiceProvider();
 
 export const main = async (event, context) => {
-  return success({
-    test: 'test',
-  });
+  const listUsersRequest = {
+    UserPoolId: process.env.USER_POOL_ID,
+    Limit: 10,
+  };
+
+  try {
+    const users = await provider.listUsers(listUsersRequest).promise();
+    console.log(JSON.stringify(users));
+
+    return success({
+      users: users.Users ? users.Users : [],
+      token: users.PaginationToken ? users.PaginationToken : null,
+    });
+  } catch (err) {
+    return internalServerError(err.message);
+  }
 };
